ea068e6ea595f73eb2953e9bf21a7409eebba7b8
Rahul Sachan <r.sachan2511@gmail.com>
2020-10-22 13:44:41 +0530

    Added basic proto schema and server implementataion

---
+++ greet/greet_server/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+}
